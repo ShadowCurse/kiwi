3fedffbec4cce18da3a23fe6431f3731aa9cd8b3
ShadowCurse <shadowcurse57@gmail.com>
2022-10-31 16:54:15 +0300

    basic project setup

---
+++ src/entity.rs
@@ -0,0 +1,47 @@
+const MAX_ENTITIES: u16 = std::u16::MAX;
+
+type EntityGeneration = u16;
+type EntityId = u16;
+
+#[derive(Debug, Eq, PartialEq, Hash)]
+pub struct Entity {
+    id: u16,
+    gen: u16,
+}
+
+#[derive(Debug)]
+pub struct EntityGenerator {
+    generations: Vec<EntityGeneration>,
+    pending: Vec<EntityId>,
+}
+
+impl EntityGenerator {
+    pub fn new() -> Self {
+        Self {
+            generations: Vec::with_capacity(MAX_ENTITIES as usize),
+            pending: Vec::with_capacity(MAX_ENTITIES as usize),
+        }
+    }
+
+    pub fn create(&mut self) -> Entity {
+        if let Some(id) = self.pending.pop() {
+            Entity {
+                id,
+                gen: self.generations[id as usize],
+            }
+        } else {
+            let id = self.generations.len() as u16;
+            self.generations.push(0);
+            Entity { id, gen: 0 }
+        }
+    }
+
+    pub fn delete(&mut self, e: &Entity) {
+        let generation = self.generations.get_mut(e.id as usize).unwrap();
+        if *generation != e.gen {
+            return;
+        }
+        *generation += 1;
+        self.pending.push(e.id);
+    }
+}
