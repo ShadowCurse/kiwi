32b2063109625cefe642af96a829df44295d541d
ShadowCurse <shadowcurse57@gmail.com>
2022-11-03 14:14:01 +0300

    initial table storage implementation

--- src/table.rs
+++ src/table.rs
@@ -1,6 +1,8 @@
-use std::{collections::HashMap, any::TypeId};
+use std::{any::TypeId, collections::HashMap};
 
-use crate::{sparse_set::SparseSet, blobvec::BlobVec};
+use crate::{
+    archetype::ComponentInfo, blobvec::BlobVec, sparse_set::SparseSet, Archetype, EcsError,
+};
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 pub struct TableId(usize);
@@ -11,16 +13,52 @@ pub struct TableStorage {
 }
 
 impl TableStorage {
-    pub fn create(&mut self) -> TableId {
+    pub fn new_table(&mut self) -> TableId {
+        TableId(self.tables.insert(Table::default()))
+    }
+
+    pub fn assign_archetype(
+        &mut self,
+        table_id: TableId,
+        archetype: &Archetype,
+    ) -> Result<(), EcsError> {
+        match self.tables.get_mut(table_id.0) {
+            Some(table) => {
+                if !table.columns.is_empty() {
+                    Err(EcsError::TableAlreadyAssignedArchetype)
+                } else {
+                    // TODO maybe there is a better way
+                    // Actually archetype should not containt dublicated componens
+                    match archetype
+                        .iter()
+                        .map(|type_info| table.register_component(type_info))
+                        .collect::<Result<Vec<_>, EcsError>>()
+                    {
+                        Ok(_) => Ok(()),
+                        Err(e) => {
+                            table.clear();
+                            Err(e)
+                        }
+                    }
+                }
+            }
+            None => Err(EcsError::TableDoesNotExist),
+        }
+    }
+
+    pub fn transfer_components(&mut self, from: TableId, to: TableId) -> Result<(), EcsError> {
         todo!()
     }
 
-    pub fn transfer_components(&mut self, from: TableId, to: TableId) -> TableId {
-        todo!()
-    }
-
-    pub fn insert_component<T>(&mut self, table_id: TableId, component: T) -> TableId {
-        todo!()
+    pub fn insert_component<T: 'static>(
+        &mut self,
+        table_id: TableId,
+        component: T,
+    ) -> Result<(), EcsError> {
+        match self.tables.get_mut(table_id.0) {
+            Some(table) => table.insert_component(component),
+            None => Err(EcsError::TableDoesNotExist),
+        }
     }
 }
 
@@ -29,3 +67,34 @@ pub struct Table {
     columns: HashMap<TypeId, BlobVec>,
 }
 
+impl Table {
+    pub fn clear(&mut self) {
+        self.columns.clear();
+    }
+
+    pub fn register_component(&mut self, component_info: &ComponentInfo) -> Result<(), EcsError> {
+        match self.columns.contains_key(&component_info.id) {
+            false => {
+                self.columns
+                    .insert(component_info.id, BlobVec::new(component_info.layout));
+                Ok(())
+            }
+            true => Err(EcsError::TableRegisteringDuplicatedComponent),
+        }
+    }
+
+    pub fn insert_component<T: 'static>(&mut self, component: T) -> Result<(), EcsError> {
+        let type_id = TypeId::of::<T>();
+        match self.columns.get_mut(&type_id) {
+            Some(column) => {
+                // If column exist for the type
+                // then it is safe to add component of this type
+                unsafe {
+                    column.add(component);
+                }
+                Ok(())
+            }
+            None => Err(EcsError::TableDoesNotContainComponentColumn),
+        }
+    }
+}
