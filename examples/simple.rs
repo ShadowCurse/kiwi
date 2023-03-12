28476d225359c6802d5b631637ba4f8cb7a0c8a7
ShadowCurse <shadowcurse57@gmail.com>
2023-03-12 17:33:16 +0000

    updatet example with WorldRef and startup system

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,4 +1,9 @@
-use kiwi::{component::Component, query::Query, system::Systems, world::World};
+use kiwi::{
+    component::Component,
+    query::Query,
+    system::Systems,
+    world::{World, WorldRefMut},
+};
 
 #[derive(Debug)]
 struct Position {
@@ -14,6 +19,32 @@ struct Velocity {
 }
 
 impl Component for Velocity {}
+
+fn setup(mut world: WorldRefMut) {
+    let ball1 = world.create();
+    world
+        .add_component(ball1, Position { x: 1.0, y: 1.0 })
+        .unwrap();
+    world
+        .add_component(ball1, Velocity { x: 1.0, y: 0.0 })
+        .unwrap();
+
+    let ball2 = world.create();
+    world
+        .add_component(ball2, Position { x: 4.0, y: 1.0 })
+        .unwrap();
+    world
+        .add_component(ball2, Velocity { x: 1.0, y: -1.0 })
+        .unwrap();
+
+    let ball3 = world.create();
+    world
+        .add_component(ball3, Position { x: -3.0, y: 1.0 })
+        .unwrap();
+    world
+        .add_component(ball3, Velocity { x: -1.0, y: 3.0 })
+        .unwrap();
+}
 
 fn update_ball(query: Query<(&mut Position, &mut Velocity), 2>) {
     for (pos, vel) in query.iter() {
@@ -37,26 +68,9 @@ fn print_ball(query: Query<(&Position,), 1>) {
 fn main() {
     let mut ecs = World::default();
 
-    let ball1 = ecs.create();
-    ecs.add_component(ball1, Position { x: 1.0, y: 1.0 })
-        .unwrap();
-    ecs.add_component(ball1, Velocity { x: 1.0, y: 0.0 })
-        .unwrap();
-
-    let ball2 = ecs.create();
-    ecs.add_component(ball2, Position { x: 4.0, y: 1.0 })
-        .unwrap();
-    ecs.add_component(ball2, Velocity { x: 1.0, y: -1.0 })
-        .unwrap();
-
-    let ball3 = ecs.create();
-    ecs.add_component(ball3, Position { x: -3.0, y: 1.0 })
-        .unwrap();
-    ecs.add_component(ball3, Velocity { x: -1.0, y: 3.0 })
-        .unwrap();
-
     let mut systems = Systems::default();
 
+    systems.add_startup_system(setup);
     systems.add_system(update_ball);
     systems.add_system(print_ball);
 
